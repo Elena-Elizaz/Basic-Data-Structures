9bd196da111e701b12867af2cd90cab173f1eb20
Elena-Elizaz <opryatova96@mail.ru>
2024-07-18 21:08:41 +1000

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,18 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(1234%1000//10+5678%1000//10)
+#4th program
+print(int(13.42)==13) and (int(42.13)==42)
+print(int(13.42)==13) or (int(42.13)==42)
+
+
+
+
+
+
+
+
+
