779b6bfae0f5d566e290aeeff6df80930bc48382
Elena-Elizaz <opryatova96@mail.ru>
2024-07-19 18:29:02 +1000

    Add files via upload

---
+++ homework0.py
@@ -0,0 +1,25 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(1234%1000//10+5678%1000//10)
+#или:
+a=1234%1000//10
+b=5678%1000//10
+print(a+b)
+#4th program
+a, b = 13.42, 42.13
+a1, b1 =int(a), int(b)
+a2, b2 =int((a - a1) * 100), int((b - b1) * 100)
+print(a1 == b2 or b1 == a2)
+
+
+
+
+
+
+
+
+
+
